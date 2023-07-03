d826efc17d16a41abf803b06a04eefb8f08aef04
Christian Meusel <christian.meusel@posteo.de>
2023-07-04 00:54:08 +0200

    Add a bunch of CLI options to continuous sender

--- src/bin/contninuous-send.rs
+++ src/bin/contninuous-send.rs
@@ -1,10 +1,21 @@
 use clap::Parser;
+use serialport::ClearBuffer;
+use std::time::Duration;
 
 #[derive(Debug, Parser)]
 pub struct AppConfig {
     /// The baud rate to send data at.
     #[arg(long, default_value_t = 115200)]
     baud: u32,
+    /// Timeout for writing a single pattern in ms.
+    #[arg(long)]
+    timeout_ms: Option<u64>,
+    /// Clear buffers before initially writing data.
+    #[arg(long, default_value_t = true)]
+    clear: bool,
+    /// Flush output after writing the pattern once.
+    #[arg(long, default_value_t = false)]
+    flush: bool,
     /// The serial device to send data with.
     device: String,
 }
@@ -16,9 +27,23 @@ fn main() {
         .open()
         .unwrap();
 
+    println!("port: {:?}", port);
+    println!("timeout: {:?}", port.timeout());
+
     let pattern = b" 0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
+
+    if let Some(timeout_ms) = config.timeout_ms {
+        port.set_timeout(Duration::from_millis(timeout_ms)).unwrap();
+    }
+
+    if config.clear {
+        port.clear(ClearBuffer::All).unwrap();
+    }
 
     loop {
         port.write_all(pattern).unwrap();
+        if config.flush {
+            port.flush().unwrap();
+        }
     }
 }
